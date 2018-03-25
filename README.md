a3e5530d90bb6f5487fa189e03b4b4001bed641e
Erlimar Silva Campos <erlimar@gmail.com>
2018-03-25 18:37:04 -0300

    Adiciona codigo inicial

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # E5R.Architecture
-Componentes da arquitetura de software E5R
+Componentes da arquitetura de software E5R para .NET Standard 1.0.
+
+## E5R.Architecture.Core
+
+Componente principal, base para todos os outros.
+
+## Novidades
+
+Veja [NEWS.md](NEWS.md)
---
+++ test/E5R.Architecture.Core.Test/SemVerTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace E5R.Architecture.Core.Test
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(0,0,0, null, "0.0.0")]
+        [InlineData(1,0,0, null, "1.0.0")]
+        [InlineData(0,1,0, null, "0.1.0")]
+        [InlineData(0,0,1, null, "0.0.1")]
+        [InlineData(1,0,0, "build", "1.0.0-build")]
+        [InlineData(1,0,0, "alpha-4", "1.0.0-alpha-4")]
+        [InlineData(1,0,0, "ci-build-999", "1.0.0-ci-build-999")]
+        public void Should_Return_Matching_String(int major, int minor, int patch, string label, string version)
+        {
+            var semver = new SemVer { Major = major, Minor = minor, Patch = patch, Label = label };
+            Assert.Equal(version, semver.ToString());
+        }
+    }
+}
