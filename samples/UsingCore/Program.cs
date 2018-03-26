208f0030e6d620b619672dc129ef42909a2494da
Erlimar Silva Campos <erlimar@gmail.com>
2018-03-26 19:45:00 -0300

    Adiciona exemplo para uso de E5R.Architecture.Core

---
+++ samples/UsingCore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using E5R.Architecture.Core;
+
+namespace UsingCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var version = new SemVer
+            {
+                Major = 0,
+                Minor = 1,
+                Label = "alpha"
+            };
+
+            Console.WriteLine($"Hello World v{version}!");
+        }
+    }
+}
