0ae5330c3af5aa78b4a135e86c3f0bb9834b4959
Erlimar Silva Campos <erlimar@gmail.com>
2018-04-04 13:08:24 -0300

    Adiciona ordenador de dados

--- src/E5R.Architecture.Data/DataSorter.cs
+++ src/E5R.Architecture.Data/DataSorter.cs
@@ -1,11 +1,11 @@
 ﻿namespace E5R.Architecture.Data
 {
     /// <summary>
-    /// Is a #skip-take
+    /// Is a #order-by
     /// </summary>
     /// <typeparam name="TModel">Model type</typeparam>
     /// <typeparam name="TIdentifier">Identifier type of model</typeparam>
-    public class DataLimiter<TModel, TIdentifier> : DataSorter<TModel, TIdentifier>
+    public class DataSorter<TModel, TIdentifier>
         where TModel : DataModel<TIdentifier>
         where TIdentifier : struct
     {
