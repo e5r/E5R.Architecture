2d010051580a88a5d8f17876f96234f742348982
Erlimar Silva Campos <erlimar@gmail.com>
2018-04-04 13:08:41 -0300

    Documenta tipos de dados

--- src/E5R.Architecture.Data/DataModel.cs
+++ src/E5R.Architecture.Data/DataModel.cs
@@ -2,6 +2,10 @@
 
 namespace E5R.Architecture.Data
 {
+    /// <summary>
+    /// Is a #entity
+    /// </summary>
+    /// <typeparam name="TIdentifier">Identifier type of model</typeparam>
     public class DataModel<TIdentifier> where TIdentifier : struct
     {
         private readonly TIdentifier? _identifier;
--- src/E5R.Architecture.Data/DataReducer.cs
+++ src/E5R.Architecture.Data/DataReducer.cs
@@ -1,5 +1,10 @@
 ﻿namespace E5R.Architecture.Data
 {
+    /// <summary>
+    /// Is a #where
+    /// </summary>
+    /// <typeparam name="TModel">Model type</typeparam>
+    /// <typeparam name="TIdentifier">Identifier type of model</typeparam>
     public class DataReducer<TModel, TIdentifier>
         where TModel : DataModel<TIdentifier>
         where TIdentifier : struct
