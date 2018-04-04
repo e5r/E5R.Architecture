6a4e2e9a0d593fbc7460768ff82dd8a4cf19c1b6
Erlimar Silva Campos <erlimar@gmail.com>
2018-04-04 19:01:52 -0300

    Refatora e adiciona testes para DataReducer

---
+++ src/E5R.Architecture.Data/DataReducer`2.cs
@@ -0,0 +1,13 @@
+﻿namespace E5R.Architecture.Data
+{
+    /// <summary>
+    /// Data reducer (Where) for data model with identifier
+    /// </summary>
+    /// <typeparam name="TModel">Model type</typeparam>
+    /// <typeparam name="TIdentifier">Identifier type of model</typeparam>
+    public class DataReducer<TModel, TIdentifier>
+        where TModel : DataModel<TIdentifier>
+        where TIdentifier : struct
+    {
+    }
+}
