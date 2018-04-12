31ea6fb1db855b01e018ac4dcff6c1658f859d34
Erlimar Silva Campos <erlimar@gmail.com>
2018-04-12 03:01:29 -0300

    Implementa UnitOfWork para EntityFramework

--- src/E5R.Architecture.Data.EntityFrameworkCore/UnitOfWork.cs
+++ src/E5R.Architecture.Data.EntityFrameworkCore/UnitOfWork.cs
@@ -1,36 +1,75 @@
 ﻿using System;
 using E5R.Architecture.Data.Abstractions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
 
 namespace E5R.Architecture.Data.EntityFrameworkCore
 {
     public class UnitOfWork<TContext> : IUnitOfWork
         where TContext : DbContext
     {
-        private UnderlyingSession _session;
         private readonly TContext _context;
+
+        private UnderlyingSession _session;
+        private bool _disposed;
+        private bool _sessionStarted;
 
         public UnitOfWork(TContext context)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
-            _session = null;
         }
 
         public void SaveWork()
         {
             if (_context.ChangeTracker.HasChanges())
             {
-                _session.Get<IDbContextTransaction>().Commit();
+                _context.Database.CurrentTransaction.Commit();
+            }
+
+            _session = null;
+            _sessionStarted = false;
+        }
+
+        public UnderlyingSession Session
+        {
+            get
+            {
+                if (_session == null)
+                {
+                    _session = new UnderlyingSession(_context);
+                }
+
+                if (_sessionStarted)
+                {
+                    return _session;
+                }
+
+                _context.Database.BeginTransaction();
+                _sessionStarted = true;
+
+                return _session;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            _disposed = true;
+
+            if (!disposing) return;
+
+            if (_context.ChangeTracker.HasChanges())
+            {
+                _context.Database.CurrentTransaction.Rollback();
             }
 
             _session = null;
         }
-
-        public UnderlyingSession Session =>
-            _session ??
-            (_session =
-                new UnderlyingSession(_context.Database
-                    .BeginTransaction()));
     }
 }
