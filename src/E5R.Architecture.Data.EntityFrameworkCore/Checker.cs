011e9ca2ce7a886f0b113e3afec4f297efe4b6af
Erlimar Silva Campos <erlimar@gmail.com>
2018-04-12 03:02:00 -0300

    Inicia implementação de StorageReader para EntityFrameworkCore

---
+++ src/E5R.Architecture.Data.EntityFrameworkCore/Checker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace E5R.Architecture.Data.EntityFrameworkCore
+{
+    internal class Checker
+    {
+        public static void NotNullContext(DbContext context)
+        {
+            if (context == null)
+            {
+                // TODO: Implementar internacionalização
+                throw new NullReferenceException(
+                    $"The context is null. The session has not been configured.");
+            }
+        }
+
+        public static void NotNullArgument(object argObj, string argName)
+        {
+            if (argObj == null)
+            {
+                // TODO: Implementar internacionalização
+                throw new ArgumentNullException(argName);
+            }
+        }
+    }
+}
---
+++ src/E5R.Architecture.Data.EntityFrameworkCore/StorageReader`2.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using E5R.Architecture.Data.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace E5R.Architecture.Data.EntityFrameworkCore
+{
+    public class
+        StorageReader<TModel, TIdentifier> : IStorageReader<StorageReader<TModel, TIdentifier>,
+            TModel, TIdentifier>
+        where TModel : DataModel<TIdentifier>
+        where TIdentifier : struct
+    {
+        private DbContext _context;
+        private IQueryable<TModel> _query;
+
+        public StorageReader<TModel, TIdentifier> ConfigureSession(UnderlyingSession session)
+        {
+            Checker.NotNullArgument(session, nameof(session));
+
+            _context = session.Get<DbContext>();
+            _query = _context.Set<TModel>().AsNoTracking();
+
+            return this;
+        }
+
+        public TModel Find(TIdentifier id)
+        {
+            return _query.SingleOrDefault(w => w.Identifier.Equals(id));
+        }
+
+        public IEnumerable<TModel> Get(DataLimiter<TModel, TIdentifier> limiter)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<TModel> Search(DataReducer<TModel, TIdentifier> reducer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<TModel> LimitedSearch(DataReducer<TModel, TIdentifier> reducer,
+            DataLimiter<TModel, TIdentifier> limiter)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
