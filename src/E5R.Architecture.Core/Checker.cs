a4aa72d3ddf5798e65ead1f1f6aa5b34b085eb32
Erlimar Silva Campos <erlimar@gmail.com>
2018-04-14 02:59:20 -0300

    Adiciona Checker para componente Core

---
+++ src/E5R.Architecture.Core/Checker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace E5R.Architecture.Core
+{
+    public class Checker
+    {
+        public static void NotNullArgument(object argObj, string argName)
+        {
+            if (argObj == null)
+            {
+                // TODO: Implementar internacionalização
+                throw new ArgumentNullException(argName);
+            }
+        }
+    }
+}
