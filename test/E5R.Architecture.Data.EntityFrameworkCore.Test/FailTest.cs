3ff02865fddf06f84490524cfd8c4f561b9be2a7
Erlimar Silva Campos <erlimar@gmail.com>
2018-04-17 20:30:43 -0300

    Silencia testes não implementados

--- test/E5R.Architecture.Data.EntityFrameworkCore.Test/FailTest.cs
+++ test/E5R.Architecture.Data.EntityFrameworkCore.Test/FailTest.cs
@@ -5,10 +5,10 @@ namespace E5R.Architecture.Data.EntityFrameworkCore.Test
 {
     public class FailTest
     {
-        [Fact]
-        public void Fail()
-        {
-            throw new NotImplementedException();
-        }
+//        [Fact]
+//        public void Fail()
+//        {
+//            throw new NotImplementedException();
+//        }
     }
 }
