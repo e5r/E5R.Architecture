863d0bcaf44f31b45d70023d4b1c03e47aee3444
Erlimar Silva Campos <erlimar@gmail.com>
2018-05-15 13:14:58 -0300

    Adiciona informações de licença

--- src/E5R.Architecture.Business/BusinessObject.cs
+++ src/E5R.Architecture.Business/BusinessObject.cs
@@ -1,4 +1,8 @@
-﻿namespace E5R.Architecture.Business
+﻿// Copyright (c) E5R Development Team. All rights reserved.
+// This file is a part of E5R.Architecture.
+// Licensed under the Apache version 2.0: https://github.com/e5r/licenses/blob/master/license/APACHE-2.0.txt
+
+namespace E5R.Architecture.Business
 {
     using Core;
     using Data.Abstractions;
--- src/E5R.Architecture.Core/Abstractions/IIoCContainer.cs
+++ src/E5R.Architecture.Core/Abstractions/IIoCContainer.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) E5R Development Team. All rights reserved.
+// This file is a part of E5R.Architecture.
+// Licensed under the Apache version 2.0: https://github.com/e5r/licenses/blob/master/license/APACHE-2.0.txt
+
+using System;
 
 namespace E5R.Architecture.Core.Abstractions
 {
