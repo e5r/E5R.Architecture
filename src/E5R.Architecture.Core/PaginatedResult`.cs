3a3a4cc6acca51f88af013bcc231832a125528b1
Erlimar Silva Campos <erlimar@gmail.com>
2020-10-19 13:32:34 -0300

    Implementa tipo genérico para paginação

---
+++ src/E5R.Architecture.Core/PaginatedResult`.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) E5R Development Team. All rights reserved.
+// This file is a part of E5R.Architecture.
+// Licensed under the Apache version 2.0: https://github.com/e5r/licenses/blob/master/license/APACHE-2.0.txt
+
+using System;
+using System.Collections.Generic;
+
+namespace E5R.Architecture.Core
+{
+    /// <summary>
+    /// Generic paginated result of items
+    /// </summary>
+    /// <typeparam name="TResult">Result item type</typeparam>
+    public class PaginatedResult<TResult>
+    {
+        public PaginatedResult(IEnumerable<TResult> result, uint offset, uint limit, int total)
+        {
+            Checker.NotNullArgument(result, nameof(result));
+
+            Result = result;
+            Offset = offset;
+            Limit = limit;
+            Total = total;
+        }
+
+        // TODO: Rever conceito de Offset/Limit e Page
+
+        public IEnumerable<TResult> Result { get; private set; }
+        public uint Offset { get; private set; }
+        public uint Limit { get; private set; }
+        public int Total { get; private set; }
+        public int PageCount => Limit > 0
+            ? Convert.ToInt32(Math.Ceiling((double)Total / Limit))
+            : 0;
+        public int CurrentPage => Limit > 0
+            ? Convert.ToInt32(Math.Floor((double)Offset / Limit)) + 1
+            : 0;
+        public int? NextPage => CurrentPage < PageCount
+            ? CurrentPage + 1
+            : (int?)null;
+        public int? PreviousPage => CurrentPage > 1
+            ? CurrentPage - 1
+            : (int?)null;
+    }
+}
--- src/E5R.Architecture.Data/DataLimiterResult`1.cs
+++ src/E5R.Architecture.Data/DataLimiterResult`1.cs
@@ -2,7 +2,6 @@
 // This file is a part of E5R.Architecture.
 // Licensed under the Apache version 2.0: https://github.com/e5r/licenses/blob/master/license/APACHE-2.0.txt
 
-using System;
 using System.Collections.Generic;
 using E5R.Architecture.Core;
 using E5R.Architecture.Data.Abstractions;
@@ -13,67 +12,11 @@ namespace E5R.Architecture.Data
     /// Result of a search with <see cref="IDataLimiter{TDataModel}"/>
     /// </summary>
     /// <typeparam name="TDataModel">Data model type</typeparam>
-    public class DataLimiterResult<TDataModel>
+    public class DataLimiterResult<TDataModel> : PaginatedResult<TDataModel>
         where TDataModel : IDataModel
     {
         public DataLimiterResult(IEnumerable<TDataModel> result, uint offset, uint limit, int total)
-        {
-            Checker.NotNullArgument(result, nameof(result));
-
-            Result = result;
-            Offset = offset;
-            Limit = limit;
-            Total = total;
-        }
-
-        // TODO: Rever conceito de Offset/Limit e Page
-
-        public IEnumerable<TDataModel> Result { get; private set; }
-        public uint Offset { get; private set; }
-        public uint Limit { get; private set; }
-        public int Total { get; private set; }
-        public int PageCount
-        {
-            get
-            {
-                if (Limit < 1)
-                {
-                    return 0;
-                }
-                return Convert.ToInt32(Math.Ceiling((double)Total / Limit));
-            }
-        }
-        public int CurrentPage
-        {
-            get
-            {
-                if (Limit < 1)
-                {
-                    return 0;
-                }
-                return Convert.ToInt32(Math.Floor((double)Offset / Limit)) + 1;
-            }
-        }
-        public int? NextPage
-        {
-            get
-            {
-                if (CurrentPage >= PageCount)
-                    return null;
-
-                return CurrentPage + 1;
-            }
-        }
-        public int? PreviousPage
-        {
-            get
-            {
-                if (CurrentPage <= 1)
-                    return null;
-
-                return CurrentPage - 1;
-            }
-        }
-
+            : base(result, offset, limit, total)
+        { }
     }
 }
