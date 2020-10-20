3a965a96e8dda58e8000ae6efcf445547c474141
Erlimar Silva Campos <erlimar@gmail.com>
2020-10-20 17:37:06 -0300

    Consolida conceito de RuleFor<> e RuleSet<>

--- src/E5R.Architecture.Core/RuleFor.cs
+++ src/E5R.Architecture.Core/RuleFor.cs
@@ -2,15 +2,61 @@
 // This file is a part of E5R.Architecture.
 // Licensed under the Apache version 2.0: https://github.com/e5r/licenses/blob/master/license/APACHE-2.0.txt
 
-using System;
+using E5R.Architecture.Core.Exceptions;
 
 namespace E5R.Architecture.Core
 {
-    public class RuleFor<TTarget> : IRule
+    /// <summary>
+    /// Rule for a target model
+    /// <example>
+    /// </example>
+    /// Suppose you have a simple model like this:
+    /// <code>
+    /// public class MyModel
+    /// {
+    ///     public int Number { get; set; }
+    ///     public string Name { get; set; }
+    /// }
+    /// </code>
+    /// 
+    /// And according to your rules, the number must be between 1 and 99, and the name
+    /// must be "Brazil". You can express this in a rule like this:
+    /// <code>
+    /// public class MyRule1 : RuleFor<MyModel>
+    /// {
+    ///     public MyRule1()
+    ///         : base("RN-1", "My rule 1")
+    ///     { }
+    /// 
+    ///     protected override bool Check(MyModel target)
+    ///     {
+    ///         if (target.Number < 1 || target.Number > 99)
+    ///             return false;
+    /// 
+    ///         if (target.Name != "Brazil")
+    ///             return false;
+    /// 
+    ///         return true;
+    ///     }
+    /// }
+    /// </code>
+    /// 
+    /// Now that you have a defined rule, just use it in your methods
+    /// and business objects like this:
+    /// <code>
+    /// public class MyBusinessClass
+    /// {
+    ///     public void MyMethod1(MyModel model)
+    ///     {
+    ///         new MyRule1().Ensure(model);
+    ///     }
+    /// }
+    /// </code>
+    /// </summary>
+    /// <typeparam name="TTarget"></typeparam>
+    public abstract class RuleFor<TTarget> : IRule
         where TTarget : class
     {
-        private Func<TTarget, bool> _checker = (_) => throw new NotImplementedException();
-
         public RuleFor(string code, string description)
         {
             Checker.NotEmptyOrWhiteArgument(code, nameof(code));
@@ -20,17 +66,38 @@ namespace E5R.Architecture.Core
             Description = description;
         }
 
-        protected void Check(Func<TTarget, bool> checker)
-        {
-            Checker.NotNullArgument(checker, nameof(checker));
-
-            _checker = checker;
-        }
-
         public string Code { get; private set; }
 
         public string Description { get; private set; }
 
-        public bool IsOk(TTarget target) => _checker(target);
+        protected abstract bool Check(TTarget target);
+
+        /// <summary>
+        /// Ensures the object's compliance with the rules
+        /// </summary>
+        /// <param name="target">Target object instance</param>
+        /// <param name="exceptionMessageTemplate">
+        /// The exception message is usually "Rule violation {0}: {1}", where "{0}" is
+        /// the rule code, and "{1}" is the rule description.
+        /// But you can customize this message by passing the message template
+        /// in this parameter.
+        /// </param>
+        /// <exception cref="ViolatedRuleException">
+        /// When the object does not comply with the rules, an exception
+        /// </exception>
+        public void Ensure(TTarget target, string exceptionMessageTemplate = null)
+        {
+            if (!Check(target))
+            {
+                if (!string.IsNullOrWhiteSpace(exceptionMessageTemplate))
+                {
+                    throw new ViolatedRuleException(this, exceptionMessageTemplate);
+                }
+                else
+                {
+                    throw new ViolatedRuleException(this);
+                }
+            }
+        }
     }
 }
---
+++ test/E5R.Architecture.Core.Test/RuleTests.cs
@@ -0,0 +1,59 @@
+// Copyright (c) E5R Development Team. All rights reserved.
+// This file is a part of E5R.Architecture.
+// Licensed under the Apache version 2.0: https://github.com/e5r/licenses/blob/master/license/APACHE-2.0.txt
+
+using System;
+using Xunit;
+
+namespace E5R.Architecture.Core.Test
+{
+    public class RuleTests
+    {
+        [Theory]
+        [InlineData("RN1", "My businness rule 1")]
+        [InlineData("RN2", "My businness rule 2")]
+        [InlineData("RN3", "My businness rule 3")]
+        public void RuleFor_ExposesCodeAndDescription(string code, string description)
+        {
+            var rn = new DynamicRule(code, description);
+
+            Assert.Equal(code, rn.Code);
+            Assert.Equal(description, rn.Description);
+        }
+
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData("RN", null)]
+        [InlineData("RN", "")]
+        [InlineData(null, "My businness rule")]
+        [InlineData("", "My businness rule")]
+        public void RuleFor_RequiresValidCodeAndDescription(string code, string description)
+        {
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => new DynamicRule(code, description)
+            );
+
+            Assert.Matches("code|description", exception.ParamName);
+        }
+    }
+
+    #region Mocks
+    public class MyEmptyModel { }
+
+    public class IsOkRule : RuleFor<MyEmptyModel>
+    {
+        public IsOkRule() : base("RN", "IsOk RN!") { }
+        protected override bool Check(MyEmptyModel target) => true;
+    }
+
+    public class DynamicRule : RuleFor<MyEmptyModel>
+    {
+        public DynamicRule(string code, string description)
+            : base(code, description)
+        { }
+
+        protected override bool Check(MyEmptyModel target) => true;
+    }
+    #endregion
+}
