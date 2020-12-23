69407c30600058d7981a6922f8f543687193bd74
Erlimar Silva Campos <erlimar@gmail.com>
2020-12-23 20:02:48 -0300

    Adiciona sobrecarga a RawSqlRideStorage<> que usa FromSqlInterpolated() para evitar SQL injection

--- src/E5R.Architecture.Data.EntityFrameworkCore/Alias/RawSqlRideRepository.cs
+++ src/E5R.Architecture.Data.EntityFrameworkCore/Alias/RawSqlRideRepository.cs
@@ -2,8 +2,9 @@
 // This file is a part of E5R.Architecture.
 // Licensed under the Apache version 2.0: https://github.com/e5r/licenses/blob/master/license/APACHE-2.0.txt
 
-using E5R.Architecture.Data.Abstractions;
+using System;
 using Microsoft.EntityFrameworkCore;
+using E5R.Architecture.Data.Abstractions;
 
 namespace E5R.Architecture.Data.EntityFrameworkCore.Alias
 {
@@ -12,6 +13,10 @@ namespace E5R.Architecture.Data.EntityFrameworkCore.Alias
     {
         public RawSqlRideRepository(DbContext context, string sql, params object[] parameters)
             : base(context, sql, parameters)
+        { }
+
+        public RawSqlRideRepository(DbContext context, FormattableString sql)
+            : base(context, sql)
         { }
     }
 }
--- src/E5R.Architecture.Data.EntityFrameworkCore/RawSqlRideStorage.cs
+++ src/E5R.Architecture.Data.EntityFrameworkCore/RawSqlRideStorage.cs
@@ -2,8 +2,9 @@
 // This file is a part of E5R.Architecture.
 // Licensed under the Apache version 2.0: https://github.com/e5r/licenses/blob/master/license/APACHE-2.0.txt
 
-using E5R.Architecture.Data.Abstractions;
+using System;
 using Microsoft.EntityFrameworkCore;
+using E5R.Architecture.Data.Abstractions;
 
 namespace E5R.Architecture.Data.EntityFrameworkCore
 {
@@ -12,6 +13,10 @@ namespace E5R.Architecture.Data.EntityFrameworkCore
     {
         public RawSqlRideStorage(DbContext context, string sql, params object[] parameters)
             : base(context.Set<TDataModel>().FromSqlRaw(sql, parameters))
+        { }
+
+        public RawSqlRideStorage(DbContext context, FormattableString sql)
+        : base(context.Set<TDataModel>().FromSqlInterpolated(sql))
         { }
     }
 }
