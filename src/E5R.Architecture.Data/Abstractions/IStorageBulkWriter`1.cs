10baf74c68b0602297d2921662631ebf2e661b9d
Erlimar Silva Campos <erlimar@gmail.com>
2021-01-05 17:44:07 -0300

    Adiciona comentários de documentação para IStorageWriter e IStorageBulkWriter

--- src/E5R.Architecture.Data/Abstractions/IStorageBulkWriter`1.cs
+++ src/E5R.Architecture.Data/Abstractions/IStorageBulkWriter`1.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) E5R Development Team. All rights reserved.
+// Copyright (c) E5R Development Team. All rights reserved.
 // This file is a part of E5R.Architecture.
 // Licensed under the Apache version 2.0: https://github.com/e5r/licenses/blob/master/license/APACHE-2.0.txt
 
@@ -13,9 +13,33 @@ namespace E5R.Architecture.Data.Abstractions
     public interface IStorageBulkWriter<TDataModel> : IStorageSignature
         where TDataModel : IDataModel
     {
+        /// <summary>
+        /// Creates new stored objects in bulk
+        /// </summary>
+        /// <param name="data">Object data collection</param>
+        /// <returns>Created object instances</returns>
         IEnumerable<TDataModel> BulkCreate(IEnumerable<TDataModel> data);
+
+        /// <summary>
+        /// Replaces stored objects in bulk
+        /// </summary>
+        /// <param name="data">Object data collection</param>
+        /// <returns>Replaced object instances</returns>
         IEnumerable<TDataModel> BulkReplace(IEnumerable<TDataModel> data);
+
+        /// <summary>
+        /// Removes stored objects in bulk
+        /// </summary>
+        /// <param name="data">Object data collection</param>
         void BulkRemove(IEnumerable<TDataModel> data);
+
+        /// <summary>
+        /// Removes stored objects in bulk
+        /// </summary>
+        /// <remarks>
+        /// The objects to be removed are defined according to the result of a search with the <see cref="ITDataFilter" />.
+        /// </remarks
+        /// <param name="filter">Data filter</param>
         void BulkRemove(IDataFilter<TDataModel> filter);
     }
 }
--- src/E5R.Architecture.Data/Abstractions/IStorageWriter`2.cs
+++ src/E5R.Architecture.Data/Abstractions/IStorageWriter`2.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) E5R Development Team. All rights reserved.
+// Copyright (c) E5R Development Team. All rights reserved.
 // This file is a part of E5R.Architecture.
 // Licensed under the Apache version 2.0: https://github.com/e5r/licenses/blob/master/license/APACHE-2.0.txt
 
@@ -11,8 +11,30 @@ namespace E5R.Architecture.Data.Abstractions
     public interface IStorageWriter<TDataModel> : IStorageSignature
         where TDataModel : IDataModel
     {
+        /// <summary>
+        /// Creates a new stored object
+        /// </summary>
+        /// <param name="data">Object data</param>
+        /// <returns>Created object instance</returns>
         TDataModel Create(TDataModel data);
+
+        /// <summary>
+        /// Replaces a stored object
+        /// </summary>
+        /// <remarks>
+        /// The object to be replaced is defined according to the value of <see cref="TDataModel.IdentifierValues" />.
+        /// </remarks
+        /// <param name="data">Object data</param>
+        /// <returns>Replaced object instance</returns>
         TDataModel Replace(TDataModel data);
+
+        /// <summary>
+        /// Removes a stored object
+        /// </summary>
+        /// <remarks>
+        /// The object to be replaced is defined according to the value of <see cref="TDataModel.IdentifierValues" />.
+        /// </remarks
+        /// <param name="data">Object data</param>
         void Remove(TDataModel data);
     }
 }
